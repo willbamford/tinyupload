ea89cc48199ee237ddb01647c41a22ab23138f67
Will Bamford <w.bamford@gmail.com>
2017-02-09 16:58:54 +0000

    Cleanup UI and move out SVG

--- browser/src/ui/index.js
+++ browser/src/ui/index.js
@@ -1,6 +1,7 @@
 import addEmitter from '../utils/addEmitter'
 import { on } from '../utils/bindTo'
 import { stringIf, pluralIf } from '../utils/string'
+import svg from './upload.svg'
 
 const create = ({
   mount,
@@ -29,7 +30,7 @@ const create = ({
       enctype="multipart/form-data"
     >
       <div class="tu__input">
-        <svg class="tu__icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43"><path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"></path></svg>
+        ${svg}
         <input
           class="tu__file"
           type="file"
